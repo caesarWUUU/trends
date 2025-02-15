3f657bd4785d93d21d3c8abaa705a18f96392df7
caesarWUUU <karta2224537@gmail.com>
2025-02-15 15:35:36 +0800

    更新 GoogleTrends.py，調整地區為台灣並新增趨勢搜尋功能

--- trends/GoogleTrends.py
+++ trends/GoogleTrends.py
@@ -1,12 +1,11 @@
 from pytrends.request import TrendReq
-import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 pytrends = TrendReq(hl='zh-TW', tz=480)  
 
 keywords = ["Python Programming", "Data Science", "Machine Learning"]
-pytrends.build_payload(kw_list=keywords, timeframe='today 12-m', geo='US')
+pytrends.build_payload(kw_list=keywords, timeframe='today 12-m', geo='TW')
 
-data = pytrends.interest_over_time()
-print(data)
-
+trending_searches_df = pytrends.trending_searches(pn='taiwan')
+# Display trending searches
+print(trending_searches_df)
