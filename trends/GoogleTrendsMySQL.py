17ed3d628ec002404e48ee96b4045c8869e67a66
caesarWUUU <karta2224537@gmail.com>
2025-02-16 15:25:26 +0800

    新增 MySQL 整合功能，創建資料庫及資料表，並將趨勢資料存入資料庫

--- trends/GoogleTrendsMySQL.py
+++ trends/GoogleTrendsMySQL.py
@@ -7,6 +7,88 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import json
+import mysql.connector
+from mysql.connector import errorcode
+DB_NAME = 'googletrends'
+try:
+  cnx = mysql.connector.connect(user='root',password='414616',host='127.0.0.1')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  print("success connect to MySQL")
+
+cursor = cnx.cursor()
+
+#create database
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+# create table
+TABLES = {}
+TABLES['trend_dates'] = (
+    "CREATE TABLE `trend_dates` ("
+    "  `date_id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `formatted_date` date NOT NULL UNIQUE,"
+    "  PRIMARY KEY (`date_id`)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+)
+
+TABLES['trends'] = (
+    "CREATE TABLE `trends` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `date_id` int(11) NOT NULL,"
+    "  `title` varchar(100) NOT NULL,"
+    "  `searchVolume` int(11) NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX idx_date_id (`date_id`),"
+    "  FOREIGN KEY (`date_id`) REFERENCES `trend_dates` (`date_id`)"
+    "    ON DELETE CASCADE ON UPDATE CASCADE"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+)
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+# insert data
+add_date = (
+    "INSERT INTO trend_dates (formatted_date) "
+    "VALUES (%s) "
+    "ON DUPLICATE KEY UPDATE formatted_date = formatted_date"
+)
+add_trend = (
+    "INSERT INTO trends (date_id, title, searchVolume) "
+    "VALUES (%s, %s, %s)"
+)
 
 options = Options()
 options.use_chromium = True
@@ -58,7 +140,17 @@ else:
 dt = datetime.strptime(date_only, "%m月%d日")
 dt = dt.replace(year=2025)
 formatted_date = dt.strftime("%Y-%m-%d")  # 例如 "2025-02-16"
-
+# 將日期存入資料庫
+date_data = (formatted_date, )
+cursor.execute(add_date, date_data)
+cnx.commit()
+# 取得 date_id
+cursor.execute("SELECT date_id FROM trend_dates WHERE formatted_date = %s", (formatted_date,))
+result = cursor.fetchone()
+if result:
+    date_id = result[0]
+else:
+    raise Exception("無法取得日期對應的 date_id")
 # 取得所有趨勢標題與搜尋量
 titles = soup.find_all('div', class_='mZ3RIc')
 SearchVolumes = soup.find_all('div', class_='qNpYPd')
@@ -78,9 +170,20 @@ for title_tag, volume_tag in zip(titles, SearchVolumes):
 data = {
     formatted_date: trends
 }
+for trend in trends:
+    trend_data = (date_id, trend["title"], trend["searchVolume"])
+    cursor.execute(add_trend, trend_data)
+    cnx.commit()
 
-# 輸出結果（使用 JSON 格式化印出方便閱讀）
-print(json.dumps(data, ensure_ascii=False, indent=2))
+cursor.execute("SELECT * FROM trends")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
 
 input("按 Enter 鍵退出...")
 driver.quit()
+
+print("closing...")
+cursor.close()
+cnx.close()
+
